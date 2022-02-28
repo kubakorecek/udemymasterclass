c40fc480122ea5125e3e60bcd4cd3554e62cffd4
kubakorecek <jakub.korecek1@gmail.com>
2022-02-28 19:34:27 +0100

    Start Section 6.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,60 @@
 import section5.DiagonalStar;
 import section5.NumberToWords;
+import section6.Account;
+import section6.Person;
+import section6.SimpleCalculator;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        NumberToWords a = new NumberToWords();
-        System.out.println(NumberToWords.reverse(123));
-        NumberToWords.numberToWords(100);
-        NumberToWords.numberToWords(123);
+        if (args[0].equals("5")) {
+            NumberToWords a = new NumberToWords();
+            System.out.println(NumberToWords.reverse(123));
+            NumberToWords.numberToWords(100);
+            NumberToWords.numberToWords(123);
 
 
-        DiagonalStar.printSquareStar(5);
-        DiagonalStar.printSquareStar(8);
+            DiagonalStar.printSquareStar(5);
+            DiagonalStar.printSquareStar(8);
+        }
+
+        if (args[0].equals("6")) {
+            SimpleCalculator calculator = new SimpleCalculator();
+            calculator.setFirstNumber(5.0);
+            calculator.setSecondNumber(4);
+            System.out.println("add= " + calculator.getAdditionResult());
+            System.out.println("subtract= " + calculator.getSubtractionResult());
+            calculator.setFirstNumber(5.25);
+            calculator.setSecondNumber(0);
+            System.out.println("multiply= " + calculator.getMultiplicationResult());
+            System.out.println("divide= " + calculator.getDivisionResult());
+
+
+            Person person = new Person();
+            person.setFirstName("");   // firstName is set to empty string
+            person.setLastName("");    // lastName is set to empty string
+            person.setAge(10);
+            System.out.println("fullName= " + person.getFullName());
+            System.out.println("teen= " + person.isTeen());
+            person.setFirstName("John");    // firstName is set to John
+            person.setAge(18);
+            System.out.println("fullName= " + person.getFullName());
+            System.out.println("teen= " + person.isTeen());
+            person.setLastName("Smith");    // lastName is set to Smith
+            System.out.println("fullName= " + person.getFullName());
+
+            Account bobsAccount = new Account();
+            bobsAccount.withdrawal(100.0);
+
+            bobsAccount.deposit(50.);
+            bobsAccount.withdrawal(100.0);
+
+            bobsAccount.deposit(51.0);
+            bobsAccount.withdrawal(100.0);
+
+        }
     }
+
+
 }
---
+++ src/section6/Account.java
@@ -0,0 +1,67 @@
+package section6;
+
+public class Account {
+
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+
+    public void deposit(double depositAmount) {
+        balance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + " your current balance is " + this.balance);
+    }
+
+    public void withdrawal(double withdrawalAmount) {
+        if (this.balance - withdrawalAmount < 0) {
+            System.out.println("Only " + this.balance + " available. Withdrawal is not proccesed!");
+        } else {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " available. Remaining balance " + this.balance);
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+
+}
